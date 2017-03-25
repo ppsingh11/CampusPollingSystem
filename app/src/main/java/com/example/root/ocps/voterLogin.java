6f2cbb71ea51a947bf5af756b0c20231554ab8cf
ppsingh11 <prashantpratap.1996@rediffmail.com>
2017-03-25 12:44:33 +0530

    v_login module is under progress

--- app/src/main/java/com/example/root/ocps/voterLogin.java
+++ app/src/main/java/com/example/root/ocps/voterLogin.java
@@ -4,22 +4,55 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Created by Prashant Pratap on 26-02-2017.
  */
 
 public class voterLogin extends Activity {
+
+    Spinner spn_profile,spn_vote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.voter_login);
+
+        spn_profile = (Spinner)findViewById(R.id.c_spinner);
+        spn_vote = (Spinner)findViewById(R.id.v_spinner);
+
+
+        ArrayList<String> Values = new ArrayList<String>();
+        Values.add("Select Post");
+        Values.add("Hostel-President");
+        Values.add("Hostel-Vice President");
+        Values.add("College-President");
+        Values.add("College-Vice President");
+        ArrayAdapter<String> my_adp = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,Values);
+
+        spn_profile.setAdapter(my_adp);
+        spn_vote.setAdapter(my_adp);
+
     }
 
-    public void cand_info(View view) {
 
-        Intent cand_info = new Intent(this,candidateInfo.class);
 
-        startActivity(cand_info);
+
+    public void candidate_profile(View view) {
+
+        LinearLayout l = (LinearLayout)findViewById(R.id.profile_layout);
+        l.setVisibility(view.VISIBLE);
+
+    }
+
+    public void vote(View view) {
+
+        LinearLayout l = (LinearLayout)findViewById(R.id.vote_layout);
+        l.setVisibility(view.VISIBLE);
     }
 }
