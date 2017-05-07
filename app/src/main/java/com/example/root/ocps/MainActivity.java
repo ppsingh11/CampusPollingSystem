6dc7e4d70bcfcdb0140c8de1c3aed17d1eea18dd
ppsingh11 <prashantpratap.1996@rediffmail.com>
2017-05-07 19:53:29 +0530

    All modules are complete

--- app/src/main/java/com/example/root/ocps/MainActivity.java
+++ app/src/main/java/com/example/root/ocps/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.root.ocps;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,12 +19,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     SharedPreferences spf;
+    SharedPreferences.Editor edt;
+
+    Button v_reg,c_reg,result;
+    String V_reg,C_reg,Voting,Result;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +50,15 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        spf = getSharedPreferences("myshared", Context.MODE_PRIVATE);
+        edt = spf.edit();
 
+//////////////////////////////////////////////////////////////////////////
+        MyTask t  = new MyTask();
+        t.execute();
+
+
+        ////////////////////////////////////////////////////////////////////
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -106,6 +135,103 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    ///////////////////////////////Background/////////////////////////////////////
+
+    /////////////////////////////////Async Task/////////////////////////////////
+    class MyTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            String link = "https://onlinevotingnitp.000webhostapp.com/admin_act.php";
+
+
+            try {
+
+                URL url = new URL(link);
+                URLConnection conn = url.openConnection();
+
+
+                BufferedReader reader = new BufferedReader(new
+                        InputStreamReader(conn.getInputStream()));
+                StringBuffer sb = new StringBuffer("");
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+
+                return sb.toString();
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String sb) {
+            pd.dismiss();
+
+            try {
+
+
+                JSONArray json = new JSONArray(sb.toString());
+
+                JSONObject jsonObj = json.getJSONObject(0);
+
+               V_reg = jsonObj.getString("v_reg");
+               C_reg = jsonObj.getString("c_reg");
+               Voting  = jsonObj.getString("voting");
+                Result  = jsonObj.getString("publish_res");
+
+                edt.putString("VOTE",Voting);
+
+                v_reg = (Button)findViewById(R.id.v_reg);
+                c_reg = (Button)findViewById(R.id.nomination);
+                result = (Button)findViewById(R.id.result);
+
+                if(V_reg.equals("NO"))
+                {
+                    v_reg.setClickable(false);
+                }
+
+                if(C_reg.equals("NO"))
+                {
+                    c_reg.setClickable(false);
+                }
+
+                if(Result.equals("NO"))
+                {
+                    result.setClickable(false);
+                }
+
+            } catch (Exception e) {
+
+
+                Toast.makeText(getApplicationContext(), ""+e.toString(), Toast.LENGTH_SHORT).show();
+
+
+            }
+
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            pd = ProgressDialog.show(MainActivity.this, "", "Connecting to server..Please Wait", false);
+
+        }
+
+
+
+    }
+
+    ///////////////////////////Task End//////////////////////////////////
+
+
+
+    ///////////////////////////////////////////////////////////////////
 
     public void voter_register(View view) {
 
